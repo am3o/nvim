bb7d927b6c7f131644590ad4d82918759ddaf6ed
Joshua Braun <joshuabbraun@gmail.com>
2023-08-19 16:01:14 +0200

    fix: change shortcuts and add some description

--- lua/settings/remap.lua
+++ lua/settings/remap.lua
@@ -1,14 +1,14 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>vp", vim.cmd.Ex, { silent = true, desc = "[V]iew [P]roject" })
+vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, { silent = true, desc = "[F]ormat [F]ile" })
 
+-- Navigation
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
 vim.keymap.set("n", "<C-o>", "<C-o>zz")
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
+-- Functions
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
