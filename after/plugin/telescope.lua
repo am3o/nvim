def166b6294f721e71ccfca237780487a7e31607
Joshua Braun <joshuabbraun@gmail.com>
2023-09-04 14:08:41 +0200

    fix: change keybinding of search for helptags

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,7 +2,8 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>saf', builtin.find_files, { desc = "[S]earch [A]round [F]iles" })
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "[S]earch [H]elptags" })
+
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = "[S]earch [H]elptags" })
 
 require('telescope').setup()
 
