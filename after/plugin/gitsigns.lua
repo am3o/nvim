54d91f18483eea65c59beb8f10fa68392cc6224c
Joshua Braun <joshuabbraun@gmail.com>
2023-12-06 16:54:47 +0100

    fix: reduce gitsigns delay to 100ms

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -19,7 +19,7 @@ require('gitsigns').setup {
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-    delay = 500,
+    delay = 100,
     ignore_whitespace = false,
   },
   current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
