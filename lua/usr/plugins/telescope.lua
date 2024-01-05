2c827269085d9cf34cab60a0a95f7d0e283c6b62
Joshua Braun <joshuabbraun@gmail.com>
2024-01-05 12:23:51 +0100

    chore: bump telescope to verion 0.1.5

--- lua/usr/plugins/telescope.lua
+++ lua/usr/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.2',
+        tag = '0.1.5',
         dependencies = {
             'nvim-lua/plenary.nvim',
             'gbrlsnchs/telescope-lsp-handlers.nvim',
