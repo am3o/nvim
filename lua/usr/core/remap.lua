d6738c1e7cf98c17de7d933fb3ea6dc3a561dcf2
Joshua Braun <joshuabbraun@gmail.com>
2024-01-08 23:18:36 +0100

    feat: move snippets line by line with keymap

--- lua/usr/core/remap.lua
+++ lua/usr/core/remap.lua
@@ -5,6 +5,8 @@ vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, { silent = true, desc = "[
 vim.keymap.set("n", "<C-o>", "<C-o>zz")
 
 -- Search
-vim.keymap.set({"n", "v"}, "n", "nzz")
-vim.keymap.set({"n", "v"}, "N", "Nzz")
+vim.keymap.set({ "n", "v" }, "n", "nzz")
+vim.keymap.set({ "n", "v" }, "N", "Nzz")
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
