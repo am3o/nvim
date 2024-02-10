3fb503a845e88010f022a379de82cd79b054c5dd
Joshua Braun <joshuabbraun@gmail.com>
2024-02-10 15:26:39 +0100

    feat: add new keybinding to golang

--- lua/usr/plugins/go.lua
+++ lua/usr/plugins/go.lua
@@ -7,4 +7,12 @@ return {
 	ft = { "go", "gomod" },
 	build = ":lua require('go.install').update_all_sync()", -- if you need to install/update all binaries
 	opts = {},
+	config = function(_, opts)
+		local go = require("go")
+		go.setup(opts)
+
+		vim.keymap.set("n", "<leader>r", ":!go run .<CR>", { silent = true })
+		vim.keymap.set("n", "<leader>gif", "<CMD>GoIfErr<CR>", { silent = true })
+		vim.keymap.set("n", "<leader>gfs", "<CMD>GoFillStruct<CR>", { silent = true })
+	end,
 }
