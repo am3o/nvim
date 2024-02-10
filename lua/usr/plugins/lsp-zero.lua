7759f8a4ae805c0b5c8895fe4e924fb47af05335
Joshua Braun <joshuabbraun@gmail.com>
2024-02-10 15:31:05 +0100

    fix: remove autoformating and add new keybinding

--- lua/usr/plugins/lsp-zero.lua
+++ lua/usr/plugins/lsp-zero.lua
@@ -14,37 +14,37 @@ return {
 				info = "»",
 			})
 
-			lsp_zero.format_on_save({
-				format_opts = {
-					async = false,
-					timeout_ms = 10000,
-				},
-				servers = {
-					["gopls"] = { "go" },
-				},
-			})
-
 			lsp_zero.on_attach(function(_, bufnr)
 				local opts = { buffer = bufnr, remap = false }
 
 				vim.keymap.set("n", "gd", function()
 					vim.lsp.buf.definition()
 				end, opts)
+
 				vim.keymap.set("n", "gr", function()
 					vim.lsp.buf.references()
 				end, opts)
+
 				vim.keymap.set("n", "gi", function()
 					vim.lsp.buf.implementation()
 				end, opts)
+
 				vim.keymap.set("n", "K", function()
 					vim.lsp.buf.hover()
 				end, opts)
+
+				vim.keymap.set("i", "<C-K>", function()
+					vim.lsp.buf.signature_help()
+				end, opts)
+
 				vim.keymap.set("n", "[d", function()
 					vim.diagnostic.goto_next()
 				end, opts)
+
 				vim.keymap.set("n", "]d", function()
 					vim.diagnostic.goto_prev()
 				end, opts)
+
 				vim.keymap.set("i", "<C-h>", function()
 					vim.lsp.buf.signature_help()
 				end, opts)
@@ -52,12 +52,15 @@ return {
 				vim.keymap.set("n", "<leader>vws", function()
 					vim.lsp.buf.workspace_symbol()
 				end, opts)
+
 				vim.keymap.set("n", "<leader>vd", function()
 					vim.diagnostic.open_float()
 				end, opts)
+
 				vim.keymap.set("n", "<leader>vca", function()
 					vim.lsp.buf.code_action()
 				end, opts)
+
 				vim.keymap.set("n", "<leader>vrn", function()
 					vim.lsp.buf.rename()
 				end, opts)
@@ -77,7 +80,14 @@ return {
 			local lsp = require("lsp-zero")
 			local lspconfig = require("lspconfig")
 
-			require("mason").setup()
+			vim.keymap.set("n", "<leader>tm", "<cmd>Mason<cr>", { silent = true })
+
+			require("mason").setup({
+				ui = {
+					check_outdated_packages_on_open = true,
+					border = "rounded",
+				},
+			})
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"gopls",
