65118040638de4eb05e2aae3f0113dd1e0265391
Joshua Braun <joshuabbraun@gmail.com>
2024-02-10 16:51:03 +0100

    fix: modify file pattern for the plugin cloak

--- lua/usr/plugins/cloak.lua
+++ lua/usr/plugins/cloak.lua
@@ -1,15 +1,17 @@
 return {
 	"laytan/cloak.nvim",
 	opts = {
+		enabled = true,
 		cloak_character = "*",
 		highlight_group = "Comment",
 		cloak_length = nil, -- Provide a number if you want to hide the true length of the value.
 		try_all_patterns = true,
 		patterns = {
 			{
-				file_pattern = ".env*",
+				file_pattern = {
+					".env*",
+				},
 				cloak_pattern = "=.+",
-				replace = nil,
 			},
 		},
 	},
