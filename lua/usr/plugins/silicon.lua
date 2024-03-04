cd56fd38ab53a8ac48b55bced9e44c3b032ee9b4
Joshua Braun <joshuabbraun@gmail.com>
2024-03-04 17:42:02 +0100

    feat: add snapshot plugin

---
+++ lua/usr/plugins/silicon.lua
@@ -0,0 +1,19 @@
+return {
+	"michaelrommel/nvim-silicon",
+	lazy = true,
+	cmd = "Silicon",
+	opts = {
+		-- most of them could be overridden with other
+		-- the font settings with size and fallback font
+		font = "VictorMono NF=34",
+		-- the theme to use, depends on themes available to silicon
+		theme = "gruvbox-dark",
+		-- a string or function that defines the path to the output image
+		output = function()
+			return "./snapshot_" .. os.date("!%Y-%m-%dT%H-%M-%S") .. ".png"
+		end,
+	},
+	config = function(_, opts)
+		require("silicon").setup(opts)
+	end,
+}
