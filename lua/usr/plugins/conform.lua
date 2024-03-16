294a51c5d80968cd53fdab8425479dd5e6b0197b
Joshua Braun <joshuabbraun@gmail.com>
2024-03-16 17:40:47 +0100

    fix: extend args of yamlfmt

--- lua/usr/plugins/conform.lua
+++ lua/usr/plugins/conform.lua
@@ -8,12 +8,19 @@ return {
 			javascript = { "prettier" },
 			typescript = { "prettier" },
 			json = { "jq" },
-			-- FIXME: add yamlfmt parameter to respect empty lines
 			yaml = { "yamlfmt" },
 		},
 		format_on_save = {
 			timeout_ms = 500,
 			lsp_fallback = true,
+		},
+		formatters = {
+			yamlfmt = {
+				prepend_args = {
+					"-formatter",
+					"retain_line_breaks=true",
+				},
+			},
 		},
 		notify_on_error = true,
 		log_level = vim.log.levels.ERROR,
