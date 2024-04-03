f13863686c9da3bdfe00cbd997cba0195510bc66
Joshua Braun <joshuabbraun@gmail.com>
2024-04-03 22:12:59 +0200

    feat: change the color schema to catppuccin

--- lua/usr/plugins/color.lua
+++ lua/usr/plugins/color.lua
@@ -1,73 +1,79 @@
 return {
 	{
-		"folke/tokyonight.nvim",
+		"catppuccin/nvim",
 		enabled = true,
+		name = "catppuccin",
 		priority = 1000,
 		opts = {
-			style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-			transparent = true, -- Enable this to disable setting the background color
-			terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-			styles = {
-				comments = { italic = false },
-				keywords = { italic = false },
-				sidebars = "dark", -- style for sidebars, see below
-				floats = "dark", -- style for floating windows
+			flavour = "mocha", -- latte, frappe, macchiato, mocha
+			background = { -- :h background
+				light = "latte",
+				dark = "mocha",
 			},
-		},
-	},
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			variant = "moon", -- auto, main, moon, or dawn
-			dark_variant = "moon", -- main, moon, or dawn
-			disable_background = false,
-			dim_inactive_windows = true,
-			extend_background_behind_borders = true,
-			styles = {
-				bold = true,
-				italic = true,
-				transparency = false,
+			transparent_background = true, -- disables setting the background color.
+			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+			dim_inactive = {
+				enabled = false, -- dims the background color of inactive window
+				shade = "dark",
+				percentage = 0.15, -- percentage of the shade to apply to the inactive window
 			},
-			highlight_groups = {
-				StatusLineNc = { link = "StatusLine" },
+			no_italic = false, -- Force no italic
+			no_bold = false, -- Force no bold
+			no_underline = false, -- Force no underline
+			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+				comments = { "italic" }, -- Change the style of comments
+				conditionals = { "italic" },
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+				operators = {},
 			},
-			groups = {
-				border = "muted",
-				link = "iris",
-				panel = "surface",
-
-				error = "love",
-				hint = "iris",
-				info = "foam",
-				warn = "gold",
-
-				git_add = "foam",
-				git_change = "rose",
-				git_delete = "love",
-				git_dirty = "rose",
-				git_ignore = "muted",
-				git_merge = "iris",
-				git_rename = "pine",
-				git_stage = "iris",
-				git_text = "rose",
-				git_untracked = "subtle",
-
-				headings = {
-					h1 = "iris",
-					h2 = "foam",
-					h3 = "rose",
-					h4 = "gold",
-					h5 = "pine",
-					h6 = "foam",
+			color_overrides = {},
+			custom_highlights = {},
+			integrations = {
+				alpha = true,
+				cmp = true,
+				dap = true,
+				dap_ui = true,
+				flash = false,
+				gitsigns = true,
+				harpoon = false,
+				lsp_trouble = false,
+				mason = false,
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = { "italic" },
+						hints = { "italic" },
+						warnings = { "italic" },
+						information = { "italic" },
+					},
+					underlines = {
+						errors = { "underline" },
+						hints = { "underline" },
+						warnings = { "underline" },
+						information = { "underline" },
+					},
+					inlay_hints = {
+						background = true,
+					},
 				},
+				notify = false,
+				telescope = true,
+				treesitter = true,
+				treesitter_context = false,
 			},
 		},
 		config = function(_, opts)
-			require("rose-pine").setup(opts)
-			vim.cmd.colorscheme("rose-pine")
+			require("catppuccin").setup(opts)
+			vim.cmd.colorscheme("catppuccin")
 
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
