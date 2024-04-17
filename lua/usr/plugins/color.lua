91817f8ef9557bf47e9e5b3b4997c64e623fe2b8
Joshua Braun <joshuabbraun@gmail.com>
2024-04-17 10:00:04 +0200

    refactor: improve the format of the color schema

--- lua/usr/plugins/color.lua
+++ lua/usr/plugins/color.lua
@@ -1,82 +1,81 @@
 return {
-	{
-		"catppuccin/nvim",
-		enabled = true,
-		name = "catppuccin",
-		priority = 1000,
-		opts = {
-			flavour = "mocha", -- latte, frappe, macchiato, mocha
-			background = { -- :h background
-				light = "latte",
-				dark = "mocha",
-			},
-			transparent_background = true, -- disables setting the background color.
-			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-			dim_inactive = {
-				enabled = false, -- dims the background color of inactive window
-				shade = "dark",
-				percentage = 0.15, -- percentage of the shade to apply to the inactive window
-			},
-			no_italic = false, -- Force no italic
-			no_bold = false, -- Force no bold
-			no_underline = false, -- Force no underline
-			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-				comments = { "italic" }, -- Change the style of comments
-				conditionals = { "italic" },
-				loops = {},
-				functions = {},
-				keywords = {},
-				strings = {},
-				variables = {},
-				numbers = {},
-				booleans = {},
-				properties = {},
-				types = {},
-				operators = {},
-			},
-			color_overrides = {},
-			custom_highlights = {},
-			integrations = {
-				alpha = true,
-				cmp = true,
-				dap = true,
-				dap_ui = true,
-				flash = false,
-				gitsigns = true,
-				harpoon = false,
-				lsp_trouble = false,
-				mason = false,
-				native_lsp = {
-					enabled = true,
-					virtual_text = {
-						errors = { "italic" },
-						hints = { "italic" },
-						warnings = { "italic" },
-						information = { "italic" },
-					},
-					underlines = {
-						errors = { "underline" },
-						hints = { "underline" },
-						warnings = { "underline" },
-						information = { "underline" },
-					},
-					inlay_hints = {
-						background = true,
-					},
-				},
-				notify = false,
-				telescope = true,
-				treesitter = true,
-				treesitter_context = false,
-			},
+	"catppuccin/nvim",
+	enabled = true,
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		flavour = "mocha", -- latte, frappe, macchiato, mocha
+		background = { -- :h background
+			light = "latte",
+			dark = "mocha",
 		},
-		config = function(_, opts)
-			require("catppuccin").setup(opts)
-			vim.cmd.colorscheme("catppuccin")
-
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		end,
+		transparent_background = true, -- disables setting the background color.
+		show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+		term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+		dim_inactive = {
+			enabled = false, -- dims the background color of inactive window
+			shade = "dark",
+			percentage = 0.15, -- percentage of the shade to apply to the inactive window
+		},
+		no_italic = false, -- Force no italic
+		no_bold = false, -- Force no bold
+		no_underline = false, -- Force no underline
+		styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+			comments = { "italic" }, -- Change the style of comments
+			conditionals = { "italic" },
+			loops = {},
+			functions = {},
+			keywords = {},
+			strings = {},
+			variables = {},
+			numbers = {},
+			booleans = {},
+			properties = {},
+			types = {},
+			operators = {},
+		},
+		color_overrides = {},
+		custom_highlights = {},
+		integrations = {
+			alpha = true,
+			cmp = true,
+			dap = true,
+			dap_ui = true,
+			flash = false,
+			gitsigns = true,
+			harpoon = false,
+			lsp_trouble = false,
+			mason = false,
+			native_lsp = {
+				enabled = true,
+				virtual_text = {
+					errors = { "italic" },
+					hints = { "italic" },
+					warnings = { "italic" },
+					information = { "italic" },
+				},
+				underlines = {
+					errors = { "underline" },
+					hints = { "underline" },
+					warnings = { "underline" },
+					information = { "underline" },
+				},
+				inlay_hints = {
+					background = true,
+				},
+			},
+			notify = false,
+			telescope = true,
+			treesitter = true,
+			treesitter_context = false,
+			octo = false,
+		},
 	},
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
+		vim.cmd.colorscheme("catppuccin")
+
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	end,
 }
