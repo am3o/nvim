69cffb4809061bd281cd0bf5caa03c5c8256d860
Joshua Braun <joshuabbraun@gmail.com>
2024-05-20 20:09:53 +0200

    fix: remove unnecessary highlight-colors plugin

--- lua/usr/plugins/higlight-color.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"brenoprata10/nvim-highlight-colors",
-	enabled = true,
-	event = "VeryLazy",
-	opts = {},
-	config = function(_, opts)
-		require("nvim-highlight-colors").setup(opts)
-	end,
-}
