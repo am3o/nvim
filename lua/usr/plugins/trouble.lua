ad0b431c0135d2577b11f7a717222e902da2874f
Joshua Braun <joshuabbraun@gmail.com>
2024-06-12 17:25:44 +0200

    chore: bump trouble to newest version

--- lua/usr/plugins/trouble.lua
+++ lua/usr/plugins/trouble.lua
@@ -1,23 +1,8 @@
 return {
 	"folke/trouble.nvim",
 	enabled = true,
-	event = "VeryLazy",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
+	cmd = "Trouble",
+	keys = {
+		{ "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>", desc = "[T]oggle [T]rouble" },
 	},
-	opts = {
-		position = "bottom", -- position of the list can be: bottom, top, left, right
-		icons = true, -- use devicons for filenames
-		cycle_results = true, -- cycle item list when reaching beginning or end of list
-		auto_close = true, -- automatically close the list when you have no diagnostics
-		auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
-		auto_fold = false, -- automatically fold a file trouble list at creation
-		use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
-	},
-	config = function(_, opts)
-		local trouble = require("trouble")
-		trouble.setup(opts)
-
-		vim.keymap.set("n", "<leader>tt", trouble.toggle)
-	end,
 }
