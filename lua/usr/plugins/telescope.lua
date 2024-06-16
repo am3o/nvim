209eafc53960ecf2ec42e331ea302c573ec87d4d
Joshua Braun <joshuabbraun@gmail.com>
2024-06-16 11:11:51 +0200

    fix: remove treesitter dependency in the telescope configuration

--- lua/usr/plugins/telescope.lua
+++ lua/usr/plugins/telescope.lua
@@ -5,7 +5,6 @@ return {
 	branch = "0.1.x",
 	dependencies = {
 		{ "nvim-lua/plenary.nvim" },
-		{ "nvim-treesitter/nvim-treesitter-context" },
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
 			build = "make",
