51b5e341ae990b4f66befd349cd7cacffdbbef20
Joshua Braun <joshuabbraun@gmail.com>
2024-09-11 11:38:11 +0200

    refactor: modfiy the configuration of the packagemanager lazy

--- lua/usr/lazy.lua
+++ lua/usr/lazy.lua
@@ -1,16 +1,19 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+if not (vim.uv or vim.loop).fs_stat(path) then
+	local repo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", repo, path })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(path)
 
 -- [[ Basic keymaps ]]
 -- Set <space> as the leader key
@@ -20,9 +23,12 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<leader>tpm", "<cmd>Lazy<cr>", { silent = true })
+vim.keymap.set("n", "<leader>tm", "<cmd>Mason<cr>", { silent = true })
 
 require("lazy").setup({
-	spec = "usr.plugins",
+	spec = {
+		{ import = "usr.plugins" },
+	},
 	lockfile = vim.fn.stdpath("config") .. "/lua/usr/lazy-lock.json",
 	ui = {
 		title = "Lazy",
