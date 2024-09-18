045ae785f39efb5d97abe930961c24c7e6fd55b9
Joshua Braun <joshuabbraun@gmail.com>
2024-09-18 13:58:59 +0200

    fix: remove relation between pkl and treesitter

--- lua/usr/plugins/pkl.lua
+++ lua/usr/plugins/pkl.lua
@@ -2,9 +2,6 @@ return {
 	"apple/pkl-neovim",
 	enabled = true,
 	event = { "BufReadPre *.pkl" },
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-	},
 	build = function()
 		vim.cmd("TSInstall! pkl")
 	end,
