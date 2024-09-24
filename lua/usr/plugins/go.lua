390122190285d09c427d976abe150045500b92f0
Joshua Braun <joshuabbraun@gmail.com>
2024-09-24 11:20:18 +0200

    chore: re-format filestructure

--- lua/usr/plugins/go.lua
+++ lua/usr/plugins/go.lua
@@ -1,20 +1,22 @@
 return {
-	"ray-x/go.nvim",
-	enabled = true,
-	event = { "CmdlineEnter" },
-	ft = { "go", "gomod" },
-	dependencies = { -- optional packages
-		"ray-x/guihua.lua",
-	},
-	build = ":lua require('go.install').update_all_sync()", -- if you need to install/update all binaries
-	opts = {},
-	config = function(_, opts)
-		local go = require("go")
-		go.setup(opts)
+	{
+		"ray-x/go.nvim",
+		enabled = true,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		dependencies = {
+			"ray-x/guihua.lua",
+		},
+		build = ":lua require('go.install').update_all_sync()", -- if you need to install/update all binaries
+		opts = {},
+		config = function(_, opts)
+			require("go").setup(opts)
 
-		vim.keymap.set("n", "<leader>r", ":!go run .<CR>", { silent = true })
-		vim.keymap.set("n", "<leader>gif", "<CMD>GoIfErr<CR>", { silent = true })
-		vim.keymap.set("n", "<leader>gfs", "<CMD>GoFillStruct<CR>", { silent = true })
-		vim.keymap.set("n", "<leader>gat", "<CMD>GoAddTag<CR>", { silent = true })
-	end,
+			vim.keymap.set("n", "<leader>gr", ":!go run .<CR>", { silent = true })
+
+			vim.keymap.set("n", "<leader>gif", "<CMD>GoIfErr<CR>", { silent = true })
+			vim.keymap.set("n", "<leader>gfs", "<CMD>GoFillStruct<CR>", { silent = true })
+			vim.keymap.set("n", "<leader>gat", "<CMD>GoAddTag<CR>", { silent = true })
+		end,
+	},
 }
