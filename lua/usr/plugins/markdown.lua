8f5b2c0b47bad1c52e62b165a7d5941f32c76cd8
Joshua Braun <joshuabbraun@gmail.com>
2024-10-07 09:29:16 +0200

    chore: format markdown file

--- lua/usr/plugins/markdown.lua
+++ lua/usr/plugins/markdown.lua
@@ -1,9 +1,11 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	enabled = true,
-	ft = { "markdown" },
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = function()
-		vim.fn["mkdp#util#install"]()
-	end,
+	{
+		"iamcco/markdown-preview.nvim",
+		enabled = true,
+		ft = "markdown",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 }
