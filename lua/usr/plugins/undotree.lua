896fa1184c1c1b5b302aeac5e830533fef7f2479
Joshua Braun <joshuabbraun@gmail.com>
2025-03-19 09:39:12 +0100

    fix: replace the plugin with telescope undo

--- lua/usr/plugins/undotree.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"jiaoshijie/undotree",
-	enabled = false,
-	dependencies = "nvim-lua/plenary.nvim",
-	keys = {
-		{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
-	},
-	opts = {},
-}
