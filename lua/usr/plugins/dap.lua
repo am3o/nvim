7bb8612cbac3b381d1751d9a2fefebd09f29ca2a
Joshua Braun <joshuabbraun@gmail.com>
2025-04-24 09:33:58 +0200

    fix: change functionality of dap to several methods

--- lua/usr/plugins/dap.lua
+++ lua/usr/plugins/dap.lua
@@ -34,18 +34,10 @@ return {
 			vim.keymap.set("n", "<leader>od", dapui.toggle, { silent = true, desc = "[O]pen [D]ebugger" })
 
 			dapui.setup()
-			require("nvim-dap-virtual-text").setup({})
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				dapui.open()
-			end
 
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				dapui.close()
-			end
-
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				dapui.close()
-			end
+			dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+			dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+			dap.listeners.before.event_exited["dapui_config"] = dapui.close
 		end,
 	},
 	{
