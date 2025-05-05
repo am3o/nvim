5946fad21cdba8313be65e0a535540b23519f035
Joshua Braun <joshuabbraun@gmail.com>
2025-05-05 09:01:41 +0200

    refactor: change the key-binding mechanism of the plugin go

--- lua/usr/plugins/go.lua
+++ lua/usr/plugins/go.lua
@@ -2,21 +2,36 @@ return {
 	{
 		"ray-x/go.nvim",
 		enabled = true,
-		event = { "CmdlineEnter" },
 		ft = { "go", "gomod" },
+		event = { "CmdlineEnter" },
 		dependencies = {
 			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		keys = {
+			{
+				"<leader>gif",
+				"<CMD>GoIfErr<cr>",
+				{ desc = "Generate if condition" },
+			},
+			{
+				"<leader>gfs",
+				"<CMD>GoFillStruct<cr",
+				{ desc = "Generate struct attributes" },
+			},
+			{
+				"<leader>gat",
+				"<CMD>GoAddTag<cr>",
+				{ desc = "Generate struct tags" },
+			},
+			{
+				"<leader>grg",
+				"<CMD>GoRun<cr>",
+				{ desc = "Go Run" },
+			},
 		},
 		build = ":lua require('go.install').update_all_sync()", -- if you need to install/update all binaries
 		opts = {},
-		config = function(_, opts)
-			require("go").setup(opts)
-
-			vim.keymap.set("n", "<leader>gr", ":!go run .<CR>", { silent = true })
-
-			vim.keymap.set("n", "<leader>gif", "<CMD>GoIfErr<CR>", { silent = true })
-			vim.keymap.set("n", "<leader>gfs", "<CMD>GoFillStruct<CR>", { silent = true })
-			vim.keymap.set("n", "<leader>gat", "<CMD>GoAddTag<CR>", { silent = true })
-		end,
 	},
 }
