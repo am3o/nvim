2019e286383affb592dbeba5320e8fdd3537aaf4
Joshua Braun <joshuabbraun@gmail.com>
2025-05-06 09:09:33 +0200

    fix: modify the visual secrets length

--- lua/usr/plugins/cloak.lua
+++ lua/usr/plugins/cloak.lua
@@ -8,12 +8,13 @@ return {
 	opts = {
 		cloak_character = "*",
 		highlight_group = "Comment",
-		cloak_length = nil, -- Provide a number if you want to hide the true length of the value.
+		cloak_length = 16, -- Provide a number if you want to hide the true length of the value.
 		try_all_patterns = true,
 		patterns = {
 			{
 				file_pattern = {
-					".env*",
+					".envrc",
+					".env",
 				},
 				cloak_pattern = "=.+",
 			},
