aa613434a62219ef9c69476b2099174e0d7a1acd
Joshua Braun <joshuabbraun@gmail.com>
2025-05-07 09:48:16 +0200

    feat: create a nice opening message

--- lua/usr/core/autocmd.lua
+++ lua/usr/core/autocmd.lua
@@ -1,19 +1,37 @@
--- Create group to assign commandsaut
--- "clear = true" must be set to prevent loading an
--- auto-command repeatedly every time a file is resourced
-local formating = vim.api.nvim_create_augroup("formating", { clear = true })
+function Opening()
+	-- https://37signals.com
+	local header, content =
+		{
+			"Good day, Sir",
+		}, {
+			"Nice day for Fishin' ain't? 🎣",
+			"During times of universal deceit. Telling the truth becomes a revolutionary act.",
+			"Progress is our path, complexity builds the bridge, simplicity waits.",
+			"If you need a machine and don’t buy it, then you will ultimately find that you have paid for it and don’t have it.",
+		}
+
+	-- Seed the random number generator
+	math.randomseed(os.time())
+	local rnd = math.random(1, 100)
+	return {
+		Title = header[(rnd % #header) + 1],
+		Content = content[(rnd % #content) + 1],
+	}
+end
 
 -- Opening dialog
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
+		local message = Opening()
+
 		local opts = {
-			title = "Good day, Sir",
+			title = message.Title,
 			render = "compact",
 			stages = "slide",
 		}
-		vim.notify_once("Nice day for Fishin' ain't? 🎣", vim.log.levels.INFO, opts)
-		-- During times of universal deceit. Telling the truth becomes a revolutionary act.
+
+		vim.notify_once(message.Content, vim.log.levels.INFO, opts)
 	end,
-	group = formating,
+	group = vim.api.nvim_create_augroup("formating", { clear = true }),
 	buffer = 0,
 })
