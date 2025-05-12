70063355139e012f0b595702c3e0d22e82cc3cf3
Joshua Braun <joshuabbraun@gmail.com>
2025-05-12 12:02:05 +0200

    feat: modify the toggle mechanism and avoid unnecessary notifications

--- lua/usr/plugins/trouble.lua
+++ lua/usr/plugins/trouble.lua
@@ -3,6 +3,32 @@ return {
 	enabled = true,
 	cmd = "Trouble",
 	keys = {
-		{ "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>", desc = "[T]oggle [T]rouble" },
+		{
+			"<leader>tt",
+			function()
+				if vim.tbl_isempty(vim.diagnostic.get()) then
+					vim.notify("No diagnostic found", vim.log.levels.INFO, {
+						title = "Diagnostic",
+						render = "compact",
+						stages = "slide",
+					})
+					return
+				end
+
+				local opts = {
+					mode = "diagnostics",
+				}
+				require("trouble").toggle(opts)
+			end,
+			desc = "[T]oggle [T]rouble",
+		},
+	},
+	opts = {
+		modes = {
+			diagnostics = {
+				auto_open = false,
+				auto_close = true,
+			},
+		},
 	},
 }
