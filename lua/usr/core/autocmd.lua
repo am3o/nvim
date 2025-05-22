ac17738d9d9bbe2a091ba9a397012820fff5155d
Joshua Braun <joshuabbraun@gmail.com>
2025-05-22 19:50:08 +0200

    feat: modify the formating of the daily tip

--- lua/usr/core/autocmd.lua
+++ lua/usr/core/autocmd.lua
@@ -1,26 +1,50 @@
--- TODO: extract the messages to a better place
-local header, content =
+local message = {
 	{
-		"Good day, Sir",
-	}, {
-		"\n\t\tNice day for Fishin' ain't? 🎣",
-		"\n\t\tDuring times of universal deceit.\n\t\tTelling the truth becomes a revolutionary act.",
-		"\n\t\tProgress is our path, complexity \n\t\tbuilds the bridge, simplicity waits.",
-		"\n\t\tIf you need a machine and don’t buy it, \n\t\tthen you will ultimately find that you have \n\t\tpaid for it and don’t have it.",
-		"\n\t\t Muss net schmecke, muss wirke.",
-		"\n\t\t You miss 100% of the shots you don't take. -Wayne Gretzky",
-	}
+		header = "Good day, Sir",
+		content = "Nice day for Fishin' ain't? 🎣",
+	},
+	{
+		content = "During times of universal deceit.Telling the truth becomes a revolutionary act.",
+	},
+	{
+		content = "Progress is our path, complexity builds the bridge, simplicity waits.",
+	},
+	{
+		content = "If you need a machine and don’t buy it, then you will ultimately find that you have paid for it and don’t have it.",
+	},
+	{
+		header = "Markus Rühl",
+		content = "Muss net schmecke, muss wirke.",
+	},
+	{
+		header = "Wayne Gretzky",
+		content = "You miss 100% of the shots you don't take.",
+	},
+	{
+		header = "A few good men",
+		content = "A rooling stone gathers no moss",
+	},
+	{
+		content = "Oh Captain, Oh Captain",
+	},
+	{
+		content = "Look at me. I'M the captain now ✌️",
+	},
+}
 
--- TODO: extract the daily tip to a dedicated plugin to configure them and be able to disable it.
-function format(value, rule)
-	local rule = rule or "%s"
-
-	local result = {}
-	for str in string.gmatch(value, "([^" .. rule .. "])") do
-		table.insert(result, str)
+--- @param value any
+--- @param rule string
+--- @param length number
+function Format(value, rule, length)
+	local content = {}
+	for str in string.gmatch(value, "[^" .. rule .. "]+") do
+		table.insert(content, str)
+		if #content % length == 0 then
+			table.insert(content, "\n\t\t")
+		end
 	end
 
-	return table.concat(result, " ")
+	return table.concat(content, " ")
 end
 
 -- Opening dialog
@@ -28,15 +52,16 @@ vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
 		math.randomseed(os.time())
 		local rnd = math.random(1, 100)
+
+		local msg = message[(rnd % #message) + 1]
 		local opts = {
-			title = header[(rnd % #header) + 1],
+			title = msg.header or "Good day, Sir",
 			icon = "💢",
 			render = "compact",
 			stages = "slide",
 		}
 
-		local message = content[(rnd % #content) + 1]
-		vim.notify_once(message, vim.log.levels.INFO, opts)
+		vim.notify_once(Format(msg.content, "%s", 7), vim.log.levels.INFO, opts)
 	end,
 	group = vim.api.nvim_create_augroup("formating", { clear = true }),
 	buffer = 0,
