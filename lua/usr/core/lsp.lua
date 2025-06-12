6ce2646abd4474cbbeb8b64f2da364f3916f70d8
Joshua Braun <joshuabbraun@gmail.com>
2025-06-12 22:45:18 +0200

    feat: add lsp client attachment to lualine

--- lua/usr/core/lsp.lua
+++ lua/usr/core/lsp.lua
@@ -1,66 +1,79 @@
 vim.lsp.enable({
-	"bashls",
-	"gopls",
-	"helm_ls",
-	"lua_ls",
-	"terraform_lsp",
-	"ts_ls",
+  "bashls",
+  "gopls",
+  "helm_ls",
+  "lua_ls",
+  "terraform_lsp",
+  "ts_ls",
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(event)
-		local opts = {
-			title = "native LSP",
-			icon = "🔧",
-			render = "default",
-			stages = "slide",
-		}
-
-		local client = vim.lsp.get_client_by_id(event.data.client_id)
-		if client then
-			vim.notify("LSP attached: " .. client.name, vim.log.levels.INFO, opts)
-		end
-
-    local builtin = require("telescope.builtin")
+  group = vim.api.nvim_create_augroup("keymap.native.lsp", {}),
+  callback = function()
+    local builtin = assert(require("telescope.builtin"))
 
     vim.keymap.set("n", "gd", builtin.lsp_definitions, { remap = false })
-		vim.keymap.set("n", "gr", builtin.lsp_references, { remap = false })
-		vim.keymap.set("n", "gi", builtin.lsp_implementations, { remap = false })
-		vim.keymap.set("n", "<leader>D", builtin.lsp_type_definitions, { remap = false })
-		vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, { remap = false })
+    vim.keymap.set("n", "gr", builtin.lsp_references, { remap = false })
+    vim.keymap.set("n", "gi", builtin.lsp_implementations, { remap = false })
+    vim.keymap.set("n", "<leader>D", builtin.lsp_type_definitions, { remap = false })
+    vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, { remap = false })
 
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, { remap = false })
-		vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, { remap = false })
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { remap = false })
+    vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, { remap = false })
 
     vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { remap = false })
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { remap = false })
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { remap = false })
 
-		vim.keymap.set("n", "<leader>od", vim.diagnostic.open_float, { remap = false })
-	end,
+    vim.keymap.set("n", "<leader>od", vim.diagnostic.open_float, { remap = false })
+  end,
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(event)
-		local client = vim.lsp.get_client_by_id(event.data.client_id)
-		if client and client.server_capabilities.documentHighlightProvider then
-			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-				buffer = event.buf,
-				callback = vim.lsp.buf.document_highlight,
-			})
+  group = vim.api.nvim_create_augroup("native.lsp", {}),
+  callback = function(event)
+    local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
+    if client and client.server_capabilities.documentHighlightProvider then
+      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+        group = vim.api.nvim_create_augroup("hightlight.native.lsp", {}),
+        buffer = event.buf,
+        callback = vim.lsp.buf.document_highlight,
+      })
 
-			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-				buffer = event.buf,
-				callback = vim.lsp.buf.clear_references,
-			})
-		end
-	end,
+      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+        group = vim.api.nvim_create_augroup("unhightlight.native.lsp", {}),
+        buffer = event.buf,
+        callback = vim.lsp.buf.clear_references,
+      })
+    end
+
+    if client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup("formating.native.lsp", { clear = false }),
+        buffer = event.buf,
+        callback = function()
+          local opts = {
+            title  = "native LSP",
+            render = "compact",
+            stages = "slide",
+          }
+
+          vim.notify("auto-format... ", vim.log.levels.INFO, opts)
+          vim.lsp.buf.format({
+            bufnr = event.buf,
+            id = client.id,
+            timeout_ms = 1000,
+          })
+        end
+      })
+    end
+  end,
 })
 
 vim.diagnostic.config({
   signs = true,
-	underline = true,
-	update_in_insert = false,
-	severity_sort = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
 })
 
 -- Toggle virtual_text off when on the line with the error
--- lua/usr/plugins/lualine.lua
+++ lua/usr/plugins/lualine.lua
@@ -1,54 +1,84 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	enabled = true,
-	event = "VeryLazy",
-	dependencies = {
-		"folke/trouble.nvim",
-	},
-	opts = {
-		options = {
-			theme = "catppuccin",
-			component_separators = "|",
-			section_separators = { left = "", right = "" },
-		},
-		sections = {
-			lualine_a = {
-				{ "mode", separator = { left = "" }, right_padding = 2 },
-			},
-			lualine_b = { "filename", "branch" },
-			lualine_c = {},
-			lualine_x = {},
-			lualine_y = { "filetype", "progress" },
-			lualine_z = {
-				{ "location", separator = { right = "" }, left_padding = 2 },
-			},
-		},
-		inactive_sections = {
-			lualine_a = { "filename" },
-			lualine_b = {},
-			lualine_c = {},
-			lualine_x = {},
-			lualine_y = {},
-			lualine_z = { "location" },
-		},
-		tabline = {},
-		extensions = {},
-	},
-	config = function(_, opts)
-		local symbols = require("trouble").statusline({
-			mode = "lsp_document_symbols",
-			groups = {},
-			title = false,
-			filter = { range = true },
-			format = "{kind_icon}{symbol.name:Normal}",
-			hl_group = "lualine_c",
-		})
+  "nvim-lualine/lualine.nvim",
+  enabled = true,
+  event = "VeryLazy",
+  dependencies = {
+    "folke/trouble.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    options = {
+      theme = "catppuccin",
+      component_separators = "|",
+      section_separators = { left = "", right = "" },
+    },
+    sections = {
+      lualine_a = {
+        {
+          "mode",
+          separator = {
+            left = "",
+            right = "",
+          },
+        },
+      },
+      lualine_b = { "filename", "branch", "diagnostics" },
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {
+        {
+          "location",
+          separator = { left = "", right = "" },
+        },
+      },
+    },
+    inactive_sections = {
+      lualine_a = { "filename" },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = { "location" },
+    },
+    tabline = {},
+    extensions = {},
+  },
+  config = function(_, opts)
+    local symbols = require("trouble").statusline({
+      mode = "lsp_document_symbols",
+      groups = {},
+      title = false,
+      filter = { range = true },
+      format = "{kind_icon}{symbol.name:Normal}",
+      hl_group = "lualine_c",
+    })
 
-		table.insert(opts.sections.lualine_c, {
-			symbols.get,
-			cond = symbols.has,
-		})
+    table.insert(opts.sections.lualine_c, {
+      symbols.get,
+      cond = symbols.has,
+    })
 
-		require("lualine").setup(opts)
-	end,
+    local lsp_status = function()
+      local clients     = assert(vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() }))
+
+      local _, devicons = assert(pcall(require, 'nvim-web-devicons'))
+      local icon, _     = devicons.get_icon(vim.fn.expand('%:t'))
+      if not icon then
+        icon, _ = devicons.get_icon_by_filetype(vim.bo.filetype, { default = true })
+      end
+
+      -- Choose color based on LSP client presence
+      local color = (#clients > 0) and "DiagnosticOk" or "Comment"
+
+      return string.format("%%#%s#%s %%*", color, icon)
+    end
+
+    table.insert(opts.sections.lualine_y, {
+      lsp_status,
+      separator = {},
+    })
+
+    require("lualine").setup(opts)
+  end,
 }
