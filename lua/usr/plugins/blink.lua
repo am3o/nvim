44b40ae09597272da341bf179c43e6ba25679fc3
Joshua Braun <joshuabbraun@gmail.com>
2025-06-12 16:39:13 +0200

    feat: add new completion plugin blink

---
+++ lua/usr/plugins/blink.lua
@@ -0,0 +1,44 @@
+return {
+  'saghen/blink.cmp',
+  enabled = true,
+  version = '1.*',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = {
+      preset = 'enter',
+    },
+
+    appearance = {
+      nerd_font_variant = 'mono',
+    },
+
+    completion = {
+      menu = {
+        scrollbar = false,
+      },
+      documentation = {
+        auto_show = false,
+      },
+    },
+
+    sources = {
+      default = {
+        'lsp',
+        'path',
+        'buffer',
+      },
+    },
+
+    fuzzy = {
+      implementation = "prefer_rust_with_warning",
+      sorts = {
+        "score",
+        "sort_text",
+      }
+    },
+  },
+
+  opts_extend = { "sources.default" },
+}
