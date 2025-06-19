dda38a31c865d5039c71da57730be2fd50023eef
Joshua Braun <joshuabbraun@gmail.com>
2025-06-19 13:08:49 +0200

    refactor: extract the icon set to dedicated component

---
+++ lua/usr/plugins/icon.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "echasnovski/mini.icons",
+    enabled = true,
+    lazy = true,
+    opts = {
+      style = "glyph",
+    },
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+    enabled = true,
+    lazy = true,
+  },
+}
