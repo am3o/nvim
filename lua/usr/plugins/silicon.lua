96f4ab85e3761ffdc667004733cbd6831c8ff93a
Joshua Braun <joshuabbraun@gmail.com>
2025-08-22 14:21:46 +0200

    refactor: extend the configuration of the silicon plugin

--- lua/usr/plugins/silicon.lua
+++ lua/usr/plugins/silicon.lua
@@ -1,20 +1,17 @@
 return {
-	"michaelrommel/nvim-silicon",
-	enabled = true,
-	lazy = "VeryLazy",
-	cmd = "Silicon",
-	opts = {
-		-- most of them could be overridden with other
-		-- the font settings with size and fallback font
-		font = "VictorMono NF=34",
-		-- the theme to use, depends on themes available to silicon
-		theme = "gruvbox-dark",
-		-- a string or function that defines the path to the output image
-		output = function()
-			return "/tmp/snapshot_" .. os.date("!%Y-%m-%dT%H-%M-%S") .. ".png"
-		end,
-	},
-	config = function(_, opts)
-		require("silicon").setup(opts)
-	end,
+  "michaelrommel/nvim-silicon",
+  enabled = true,
+  lazy = "VeryLazy",
+  cmd = "Silicon",
+  opts = {
+    -- most of them could be overridden with other
+    -- the font settings with size and fallback font
+    font = "VictorMono NF=34",
+    -- the theme to use, depends on themes available to silicon
+    theme = "gruvbox-dark",
+    -- a string or function that defines the path to the output image
+    output = function()
+      return "/tmp/snapshot_" .. os.date("!%Y-%m-%dT%H-%M-%S") .. ".png"
+    end,
+  },
 }
