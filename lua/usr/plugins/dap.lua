2306d03448a998fb99add4c4487d4cd36063a7ef
Joshua Braun <joshuabbraun@gmail.com>
2025-08-22 14:22:42 +0200

    refactor: modify the key-binding of the DAP plugin

--- lua/usr/plugins/dap.lua
+++ lua/usr/plugins/dap.lua
@@ -1,61 +1,95 @@
 return {
-	{
-		"rcarriga/nvim-dap-ui",
-		enabled = true,
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"nvim-neotest/nvim-nio",
-			"theHamsta/nvim-dap-virtual-text",
-		},
-		keys = {
-			{ "<F7>", desc = "Debug jump step into" },
-			{ "<F8>", desc = "Debug jump step over" },
-			{ "<F9>", desc = "Debug jump step out" },
-			{ "<F10>", desc = "Debug jump continue" },
-			{ "<leader>b", desc = "set [B]reakpoint" },
-			{ "<leader>B", desc = "set conditionaly [B]reakpoint" },
-			{ "<leader>rd", desc = "[R]un [D]ebugger" },
-			{ "<leader>od", desc = "[O]pen [D]ebugger" },
-		},
-		config = function()
-			local dap, dapui = require("dap"), require("dapui")
+  {
+    "rcarriga/nvim-dap-ui",
+    enabled = true,
+    lazy = true,
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+      "theHamsta/nvim-dap-virtual-text",
+    },
+    keys = {
+      {
+        "<F7>",
+        function()
+          return require("dap").step_into()
+        end,
+        desc = "Debug jump step into"
+      },
+      {
+        "<F8>",
+        function()
+          return require("dap").step_over()
+        end,
+        desc = "Debug jump step over"
+      },
+      {
+        "<F9>",
+        function()
+          return require("dap").step_out()
+        end,
+        desc = "Debug jump step out"
+      },
+      {
+        "<F10>",
+        function()
+          return require("dap").continue()
+        end,
+        desc = "Debug jump continue"
+      },
+      {
+        "<leader>b",
+        function()
+          return require("dap").toggle_breakpoint()
+        end,
+        desc = "set [B]reakpoint"
+      },
+      {
+        "<leader>B",
+        function()
+          return require("dap").set_breakpoint(vim.fn.input("Breakpoint conditions: "))
+        end,
+        desc = "set conditionaly [B]reakpoint"
+      },
+      {
+        "<leader>dr",
+        function()
+          return require("dap").restart()
+        end,
+        desc = "[R]un [D]ebugger"
+      },
+      {
+        "<leader>od",
+        function()
+          return require("dapui").toggle()
+        end,
+        desc = "[O]pen [D]ebugger"
+      },
+    },
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
 
-			vim.keymap.set("n", "<F7>", dap.step_into, { silent = true, desc = "Debug jump step into" })
-			vim.keymap.set("n", "<F8>", dap.step_over, { silent = true, desc = "Debug jump step over" })
-			vim.keymap.set("n", "<F9>", dap.step_out, { silent = true, desc = "Debug jump step out" })
-			vim.keymap.set("n", "<F10>", dap.continue, { silent = true, desc = "Debug jump continue" })
+      dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+      dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+      dap.listeners.before.event_exited["dapui_config"] = dapui.close
+    end,
+  },
+  {
+    "leoluz/nvim-dap-go",
+    enabled = true,
+    ft = {
+      "go",
+      "gomod",
+    },
+    keys = {
+      { "<leader>rd",  desc = "[R]un [D]ebugger" },
+      { "<leader>rdt", desc = "[R]un [D]ebugger [T]est" },
+    },
+    config = function()
+      local dapgo = require("dap-go")
 
-			vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { silent = true, desc = "set [B]reakpoint" })
-			vim.keymap.set("n", "<leader>B", function()
-				dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-			end, { silent = true, desc = "set conditionaly [B]reakpoint" })
-
-			vim.keymap.set("n", "<leader>rd", dap.continue, { silent = true, desc = "[R]un [D]ebugger" })
-			vim.keymap.set("n", "<leader>od", dapui.toggle, { silent = true, desc = "[O]pen [D]ebugger" })
-
-			dapui.setup()
-
-			dap.listeners.after.event_initialized["dapui_config"] = dapui.open
-			dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-			dap.listeners.before.event_exited["dapui_config"] = dapui.close
-		end,
-	},
-	{
-		"leoluz/nvim-dap-go",
-		enabled = true,
-		ft = {
-			"go",
-			"gomod",
-		},
-		keys = {
-			{ "<leader>rd", desc = "[R]un [D]ebugger" },
-			{ "<leader>rdt", desc = "[R]un [D]ebugger [T]est" },
-		},
-		config = function()
-			local dapgo = require("dap-go")
-
-			dapgo.setup()
-			vim.keymap.set("n", "<leader>rdt", dapgo.debug_test, { silent = true, desc = "[R]un [D]ebugger [T]est" })
-		end,
-	},
+      dapgo.setup()
+      vim.keymap.set("n", "<leader>rdt", dapgo.debug_test, { silent = true, desc = "[R]un [D]ebugger [T]est" })
+    end,
+  },
 }
