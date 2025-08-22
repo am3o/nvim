ed36db686c9d3161757c50a92f865aa591ae4e8f
Joshua Braun <joshuabbraun@gmail.com>
2025-08-22 14:21:22 +0200

    refactor: modify the key-bindings of the tmux plugin

--- lua/usr/plugins/tmux.lua
+++ lua/usr/plugins/tmux.lua
@@ -1,19 +1,39 @@
 return {
-	"christoomey/vim-tmux-navigator",
-	enabled = true,
-	event = "VeryLazy",
-	cmd = {
-		"TmuxNavigateLeft",
-		"TmuxNavigateDown",
-		"TmuxNavigateUp",
-		"TmuxNavigateRight",
-		"TmuxNavigatePrevious",
-	},
-	keys = {
-		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-	},
+  "christoomey/vim-tmux-navigator",
+  enabled = true,
+  event = "VeryLazy",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    {
+      "<c-h>",
+      "<cmd><C-U>TmuxNavigateLeft<cr>",
+      desc = "Jump to left window"
+    },
+    {
+      "<c-j>",
+      "<cmd><C-U>TmuxNavigateDown<cr>",
+      desc = "Jump to downside window"
+    },
+    {
+      "<c-k>",
+      "<cmd><C-U>TmuxNavigateUp<cr>",
+      desc = "Jump to upside window"
+    },
+    {
+      "<c-l>",
+      "<cmd><C-U>TmuxNavigateRight<cr>",
+      desc = "Jump to right window"
+    },
+    {
+      "<c-\\>",
+      "<cmd><C-U>TmuxNavigatePrevious<cr>",
+      desc = "Jump to previous window"
+    },
+  },
 }
