26e8fbcea7d135c43fa9377a360597653e2e7900
Joshua Braun <joshuabbraun@gmail.com>
2025-08-27 12:41:31 +0200

    fix: deactivate the daily message

--- lua/usr/core/autocmd.lua
+++ lua/usr/core/autocmd.lua
@@ -1,68 +1,73 @@
 local message = {
-	{
-		header = "Good day, Sir",
-		content = "Nice day for Fishin' ain't? 🎣",
-	},
-	{
-		content = "During times of universal deceit.Telling the truth becomes a revolutionary act.",
-	},
-	{
-		content = "Progress is our path, complexity builds the bridge, simplicity waits.",
-	},
-	{
-		content = "If you need a machine and don’t buy it, then you will ultimately find that you have paid for it and don’t have it.",
-	},
-	{
-		header = "Markus Rühl",
-		content = "Muss net schmecke, muss wirke.",
-	},
-	{
-		header = "Wayne Gretzky",
-		content = "You miss 100% of the shots you don't take.",
-	},
-	{
-		header = "A few good men",
-		content = "A rooling stone gathers no moss",
-	},
-	{
-		content = "Oh Captain, Oh Captain! 👨‍✈️",
-	},
-	{
-		content = "Look at me. I'M THE CAPTAIN NOW ✌️",
-	},
+  {
+    header = "Good day, Sir",
+    content = "Nice day for Fishin' ain't? 🎣",
+  },
+  {
+    content = "During times of universal deceit.Telling the truth becomes a revolutionary act.",
+  },
+  {
+    content = "Progress is our path, complexity builds the bridge, simplicity waits.",
+  },
+  {
+    content =
+    "If you need a machine and don’t buy it, then you will ultimately find that you have paid for it and don’t have it.",
+  },
+  {
+    header = "Wayne Gretzky",
+    content = "You miss 100% of the shots you don't take.",
+  },
+  {
+    header = "A few good men",
+    content = "The truth. You can't handle the truth.",
+  },
+  {
+    header = "A few good men",
+    content = "A rooling stone gathers no moss",
+  },
+  {
+    header = "Gladiator",
+    content = "At my signal, unleash hell",
+  },
+  {
+    content = "Oh Captain, Oh Captain! 👨‍✈️",
+  },
+  {
+    header = "Captain Phillip",
+    content = "Look at me. I'M THE CAPTAIN NOW ✌️",
+  },
 }
 
 --- @param value any
 --- @param rule string
 --- @param length number
 function Format(value, rule, length)
-	local content = { "\n\t\t" }
-	for str in string.gmatch(value, "[^" .. rule .. "]+") do
-		table.insert(content, str)
-		if #content % length == 0 then
-			table.insert(content, "\n\t\t")
-		end
-	end
+  local content = { "\n\t\t" }
+  for str in string.gmatch(value, "[^" .. rule .. "]+") do
+    table.insert(content, str)
+    if #content % length == 0 then
+      table.insert(content, "\n\t\t")
+    end
+  end
 
-	return table.concat(content, " ")
+  return table.concat(content, " ")
 end
 
 -- Opening dialog
-vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		math.randomseed(os.time())
-		local rnd = math.random(1, 100)
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   callback = function()
+--     math.randomseed(os.time())
 
-		local msg = message[(rnd % #message) + 1]
-		local opts = {
-			title = msg.header or "Good day, Sir",
-			icon = "💢",
-			render = "compact",
-			stages = "slide",
-		}
+--     local msg = message[(math.random(1, 100) % #message) + 1]
+--     local opts = {
+--       title = msg.header or "Good day, Sir",
+--       icon = "💢",
+--       render = "compact",
+--       stages = "slide",
+--     }
 
-		vim.notify_once(Format(msg.content, "%s", 9), vim.log.levels.INFO, opts)
-	end,
-	group = vim.api.nvim_create_augroup("formating", { clear = true }),
-	buffer = 0,
-})
+--     vim.notify_once(Format(msg.content, "%s", 9), vim.log.levels.INFO, opts)
+--   end,
+--   group = vim.api.nvim_create_augroup("formating", { clear = true }),
+--   buffer = 0,
+-- })
