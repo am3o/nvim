34aad90e8ca131ab9aa6146dafcdcf7ad9d4445d
Joshua Braun <joshuabbraun@gmail.com>
2025-09-01 09:13:14 +0200

    chore: execute new lsp format to the file

--- lua/usr/plugins/rainbow.lua
+++ lua/usr/plugins/rainbow.lua
@@ -1,19 +1,21 @@
 return {
-	"HiPhish/rainbow-delimiters.nvim",
-	enabled = true,
-	event = "VeryLazy",
-	config = function()
-		local rainbow_delimiters = require("rainbow-delimiters")
+  "HiPhish/rainbow-delimiters.nvim",
+  enabled = true,
+  event = "VeryLazy",
+  opts = {
+  },
+  config = function()
+    local rainbow_delimiters = require("rainbow-delimiters")
 
-		require("rainbow-delimiters.setup").setup({
-			strategy = {
-				[""] = rainbow_delimiters.strategy["global"],
-				vim = rainbow_delimiters.strategy["local"],
-			},
-			query = {
-				[""] = "rainbow-delimiters",
-				lua = "rainbow-blocks",
-			},
-		})
-	end,
+    require("rainbow-delimiters.setup").setup({
+      strategy = {
+        [""] = rainbow_delimiters.strategy["global"],
+        vim = rainbow_delimiters.strategy["local"],
+      },
+      query = {
+        [""] = "rainbow-delimiters",
+        lua = "rainbow-blocks",
+      },
+    })
+  end,
 }
