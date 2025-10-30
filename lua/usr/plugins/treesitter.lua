1220cd9b694c4cbc9c62c49d9c23023138083532
Joshua Braun <joshuabbraun@gmail.com>
2025-10-30 09:00:59 +0100

    chore: execute new lsp format to the treesitter file

--- lua/usr/plugins/treesitter.lua
+++ lua/usr/plugins/treesitter.lua
@@ -1,49 +1,48 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		enabled = true,
-		event = { "BufReadPre", "BufNewFile" },
-		build = ":TSUpdate",
-		opts = {
-			treesitter = {
-				ensure_installed = {
-					"bash",
-					"go",
-					"helm",
-					"javascript",
-					"lua",
-					"make",
-					"markdown",
-					"rust",
-					"terraform",
-					"toml",
-					"typescript",
-					"vim",
-					"vimdoc",
-					"yaml",
-				},
-				sync_install = true,
-				auto_install = false,
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<C-n>",
-						node_incremental = "<C-n>",
-						scope_incremental = false,
-						node_decremental = "<bs>",
-					},
-				},
-				indent = {
-					enable = true,
-				},
-			},
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts["treesitter"])
-		end,
-	},
+  "nvim-treesitter/nvim-treesitter",
+  enabled = true,
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  opts = {
+    treesitter = {
+      ensure_installed = {
+        "bash",
+        "go",
+        "helm",
+        "javascript",
+        "lua",
+        "make",
+        "markdown",
+        "rust",
+        "terraform",
+        "toml",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
+      sync_install = true,
+      auto_install = false,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-n>",
+          node_incremental = "<C-n>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+      indent = {
+        enable = true,
+      },
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").
+        setup(opts["treesitter"])
+  end,
 }
